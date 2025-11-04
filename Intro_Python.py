ec27a9007c7e53995ac8ec2b9dc774d77d96a14b
Giri Prasad P <giriprasa@gmail.com>
2025-11-04 12:06:39 +0530

    Till list

--- Intro_Python.py
+++ Intro_Python.py
@@ -10,3 +10,49 @@ x = 100
 print(x)
 # numerical value without any single / double quotation - if it is not decimal -> int else float
 print(type(x))
+
+x1 = 10
+print(f"The value of x1 is {x1} and its datatype is {type(x1)}")
+x1 = 10.0
+print(f"The value of x1 is {x1} and its datatype is {type(x1)}")
+x1 = 1 + 4j
+print(f"The value of x1 is {x1} and its datatype is {type(x1)}")
+
+x1 = "100"
+print(f"The value of x1 is {x1} and its datatype is {type(x1)}")
+
+x1 = "()"
+print(f"The value of x1 is {x1} and its datatype is {type(x1)}")
+
+x1 = "True"
+print(f"The value of x1 is {x1} and its datatype is {type(x1)}")
+
+x1 = True
+print(f"The value of x1 is {x1} and its datatype is {type(x1)}")
+
+
+x1 = False
+print(f"The value of x1 is {x1} and its datatype is {type(x1)}")
+
+pi = ['iPhone', 'iPad', 'Tomato']
+
+print(f"List of purchased items are {pi}")
+# access with index
+print(f"The second item in the purchased list is  {pi[1]}")
+
+pi[1] = "Dell Laptop"
+print(f"The second item in the purchased list is  {pi[1]}")
+
+print(f"The updated list is {pi}")
+pi.append("Keyboard")
+print(f"The updated list is {pi}")
+pi.insert(1, "TV")
+print(f"The updated list is {pi}")
+pi.append("iPhone")
+print(f"The updated list is {pi}")
+# pi.remove("iPhone") # first occ
+# pi.pop(2) remove by index
+# pi.pop() # remove at the end
+# pi.clear() # remove all
+# del pi
+print(f"The updated list is {pi}")
