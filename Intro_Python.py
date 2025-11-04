1f343ceaa553ef257ae456923f8b6f58d2811955
Giri Prasad P <giriprasa@gmail.com>
2025-11-04 11:39:50 +0530

    datatype

---
+++ Intro_Python.py
@@ -0,0 +1,12 @@
+print("Welcome")
+
+# data types -> dynamic binding -> mentioning not required, w.r.t data -> assigns the datatype
+# int x = 10;
+# datatype variablename = value
+# variablename = value
+# w.r.t value -> int / float / complex / boolean / str 
+
+x = 100
+print(x)
+# numerical value without any single / double quotation - if it is not decimal -> int else float
+print(type(x))
