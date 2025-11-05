ae3603c57b08b91bb83389b68f778bc6a4a7fbea
Giri Prasad P <giriprasa@gmail.com>
2025-11-05 12:01:51 +0530

    write_csv

---
+++ data_pandas.py
@@ -0,0 +1,38 @@
+# panda - handline
+# series / dataframe
+# onedim / two
+#  pip install pandas
+import pandas as pd
+s1 = pd.Series([11,12,14,18,21])
+print(s1)
+
+score = {
+    "overs" : [1,2,3,4,5,6,7,8,9],
+    "TeamA" : [12,20,28,34,40,49,52,60,72],
+    "TeamB" : [8,10,38,43,47,49,52,60,73]
+}
+
+print(score)
+
+df = pd.DataFrame(score)
+print(df)
+
+df = pd.read_csv("./customers-100.csv")
+print(df)
+print(df.head(25))
+
+# df = pd.read_excel("./Book1.xlsx")
+print(df)
+
+
+score = {
+    "overs" : [1,2,3,4,5,6,7,8,9],
+    "TeamA" : [12,20,28,34,40,49,52,60,72],
+    "TeamB" : [8,10,38,43,47,49,52,60,73]
+}
+
+print(score)
+
+df = pd.DataFrame(score)
+
+df.to_csv("./score.csv", index=False)
--- numpy.libtest01.py
+++ numpy.libtest01.py
@@ -77,5 +77,78 @@ every2element = a1[::2]
 print(every2element)
 
 
+
+
 every2element = a1[1:6:2]
 print(every2element)
+
+ar1 = np.array([1,2,3,4,5,6,7,8,9])
+ar2 = ar1.reshape(3,3)
+print(ar1)
+print(ar2)
+
+
+ar1 = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
+ar2 = ar1.reshape(3, -1)
+print(ar1)
+print(ar2)
+
+ar3 = ar2.reshape(-1)
+print(ar3)
+
+ar2_T = ar2.T
+print(ar2_T)
+
+
+
+ar1 = np.array([1,2,3,4,5,6,7,8,9])
+ar2 = ar1.reshape(3,3)
+print(ar2)
+ar3 = ar2.T
+print(ar3)
+
+
+i1 = np.array([19,21,33])
+i2 = np.array([32,22,13])
+print(i1)
+print(i2)
+print("add = :",i1 + i2)
+print("sub = :",i1 - i2)
+print("mul = :",i1 * i2)
+print("div = :",i1 / i2)
+
+
+print(ar2)
+print(ar3)
+print("add = :",ar2 + ar3)
+print("sub = :",ar2 - ar3)
+print("mul = :",ar2 * ar3)
+print("div = :",ar2 / ar3)
+
+print(ar2*ar3)
+# matrix mul .dot
+print(ar2.dot(ar3))
+
+ar4 = ar2.dot(ar3)
+print(ar4[:,0:2])
+print(ar4[:,1:])
+
+ar4 = ar2.dot(ar3)
+print(ar4[0:2,:])
+print(ar4[1:,:])
+
+print(ar4[1:,1:])
+print(ar4)
+print(np.where(ar4 > 50, "Yes","No"))
+
+ar5 = ar4.reshape(-1)
+print(ar5)
+print(np.sort(ar5))
+# np.array
+# np.zeros
+# np.ones
+# np.arange
+# np.dtype
+# reshape, -1, T
+# .dot()
+# check where
